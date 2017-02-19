5ad10a0a7500d19eb8425b655948a3308187007d
philosowaffle <baileyb622@gmail.com>
2017-02-19 13:56:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.0.3',
 
     description="""PIFX is a Python library for the LIFX cloud HTTP API""",
 
