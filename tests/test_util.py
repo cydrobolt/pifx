ddcd048876b72f645ad32e5469a67882b1c915ff
philosowaffle <baileyb622@gmail.com>
2017-02-20 08:49:48 -0600

    adding unit tests for url_encode_path

--- tests/test_util.py
+++ tests/test_util.py
@@ -22,3 +22,15 @@ def test_arg_tup_to_dict():
     assert generated_dict.get('test4') == None
     assert generated_dict.get('test2') == "abc123"
     assert generated_dict.get('test3') == "abc321"
+
+def test_encode_url_path():
+    url_path = "/12345/testing/words123"
+    expected_path = "/12345/testing/words123"
+    actual_path = url_encode_path(url_path)
+    assert actual_path == expected_path
+    
+    url_path = "/label:Living Room Lights/"
+    expected_path = "/label:Living%20Room%20Lights/"
+    actual_path = url_encode_path(url_path)
+    assert actual_path == expected_path
+    
